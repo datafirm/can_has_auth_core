73cf549dd5c213cdbfa1416f6591d691542a05fd
William Smith <BrianTheCoder>
2008-05-15 16:51:09 -0500

    fixed mailer views path

--- lib/can_has_auth_core/mailers/user_mailer.rb
+++ lib/can_has_auth_core/mailers/user_mailer.rb
@@ -22,17 +22,16 @@ class UserMailer < Merb::MailController
   
   private
     alias :_orig_template_location :_template_location
-  
+
     def _template_location(action, type = nil, controller = controller_name)
       file = controller == "layout" ? "layout.#{type}" : "#{action}.#{type}"
-    
       unless Dir["#{Merb.dir_for(:mailer)}/views/#{controller}/#{action}.#{type}.*"].empty?
         _orig_template_location( action, type, controller )
       else
-        undo   = Merb.load_paths[:view].first.gsub(%r{[^/]+}, '..')
+        undo = Merb.load_paths[:view].first.gsub(%r{[^/]+}, '..')
         prefix = File.dirname(__FILE__)
         folder = 'views'
-        File.join( '.',undo, prefix,folder, controller, file )
+        File.join( '..',undo, prefix,folder, controller, file )
       end
     end
 end
